3ab2bb79140e2215e87e3ca9cf382f5401a0feb2
Mani <maniec08@gmail.com>
2019-01-20 02:22:19 -0500

    SandwichClub

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,24 +3,37 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+    TextView alsoKnownAs;
+    TextView placeOfOrigin;
+    TextView describtion;
+    TextView ingredients;
+    ImageView ingredientsIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ingredientsIv = findViewById(R.id.image_iv);
+        alsoKnownAs = findViewById(R.id.also_known_tv);
+        placeOfOrigin = findViewById(R.id.place_of_origin_tv);
+        describtion = findViewById(R.id.description_tv);
+        ingredients = findViewById(R.id.ingredients_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,11 +56,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
-
+        populateUI(sandwich);
         setTitle(sandwich.getMainName());
     }
 
@@ -56,7 +65,37 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        failSafeLoadImage(ingredientsIv, sandwich.getImage());
+        failSafeSetText(placeOfOrigin, sandwich.getPlaceOfOrigin());
+        failSafeSetText(alsoKnownAs, sandwich.getAlsoKnownAs());
+        failSafeSetText(ingredients, sandwich.getIngredients());
+        failSafeSetText(describtion, sandwich.getDescription());
+    }
 
+    private void failSafeLoadImage(ImageView imageView, String imagePath) {
+        if (imageView == null || imagePath.isEmpty()) {
+            return;
+        }
+        Picasso.with(this)
+                .load(imagePath)
+                .into(imageView);
+    }
+
+    private void failSafeSetText(TextView tv, String value) {
+        if (!value.isEmpty()) {
+            tv.setText(value);
+        } else {
+            tv.setText(R.string.not_available_text);
+        }
+
+    }
+
+    private void failSafeSetText(TextView tv, List<String> value) {
+        if (!value.isEmpty()) {
+            tv.setText(TextUtils.join(", ", value));
+        } else {
+            tv.setText(R.string.not_available_text);
+        }
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,83 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
+    private static final String TAG = JsonUtils.class.getSimpleName();
+
+    private static final String KEY_NAME = "name";
+    private static final String KEY_MAIN_NAME = "mainName";
+    private static final String KEY_ALSO_KNOWN_AS = "alsoKnownAs";
+    private static final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
+    private static final String KEY_DESCRIBTION = "description";
+    private static final String KEY_IMAGE = "image";
+    private static final String KEY_INGREDIENTS = "ingredients";
+
+    /**
+     * Parse Json Data and return Sandwich object
+     * null on any JSONException, May be
+     * Sample Struct  {"name":{"mainName":"Bosna","alsoKnownAs":["Bosner"]},"placeOfOrigin":"Austria",
+     * "description":"Bosna is a spicy Austrian fast food dish, said to have originated in either Salzburg or Linz. It is now popular all over western Austria and southern Bavaria.",
+     * "image":"https://upload.wikimedia.org/wikipedia/commons/c/ca/Bosna_mit_2_Bratw%C3%BCrsten.jpg",
+     * "ingredients":["White bread","Bratwurst","Onions","Tomato ketchup","Mustard","Curry powder"]}
+     * @param json json to parse
+     * @return Sandwich Struct
+     */
     public static Sandwich parseSandwichJson(String json) {
+        Sandwich sandwich = new Sandwich();
+        try {
+            JSONObject sandwichJson = new JSONObject(json);
+            JSONObject nameDetailsJson = sandwichJson.getJSONObject(KEY_NAME);
+            sandwich.setMainName(nameDetailsJson.optString(KEY_MAIN_NAME, ""));
+            sandwich.setAlsoKnownAs(getJsonArrayAsList(nameDetailsJson, KEY_ALSO_KNOWN_AS));
+            sandwich.setPlaceOfOrigin(sandwichJson.optString(KEY_PLACE_OF_ORIGIN, ""));
+            sandwich.setDescription(sandwichJson.optString(KEY_DESCRIBTION, ""));
+            sandwich.setImage(sandwichJson.optString(KEY_IMAGE, ""));
+            sandwich.setIngredients(getJsonArrayAsList(sandwichJson, KEY_INGREDIENTS));
+            return sandwich;
+        } catch (JSONException e) {
+            Log.d(TAG, e.getMessage());
+        }
         return null;
+    }
+
+    /**
+     * Retrieve a jsonarray for the given key and convert json array to list
+     * Empty list is returned on JSON Exception with key retrieval
+     * Skips the object, if JSON array produces jsonexception due to data types other than String
+     *
+     * @param json input json
+     * @param key  key to get the Json array
+     * @return list of strings/ empty list on JsonException
+     */
+    public static List<String> getJsonArrayAsList(JSONObject json, String key) {
+        JSONArray jsonArray;
+        try {
+            jsonArray = json.getJSONArray(key);
+        } catch (JSONException e) {
+            return new ArrayList<>();
+        }
+        List<String> returnList = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+
+                returnList.add(jsonArray.getString(i));
+            } catch (Exception e) {
+                Log.d(TAG, e.getMessage());
+            }
+        }
+
+        return returnList;
     }
 }
